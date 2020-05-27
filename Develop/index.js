e949922972173b046ef796791c79cf4692852c9b
Rocky <rrsalerno21@gmail.com>
2020-05-27 10:29:48 -0700

    made generateMarkdown async, updated index with async, added default question answers for testing

--- Develop/index.js
+++ Develop/index.js
@@ -9,21 +9,25 @@ const questions = [
     {
         type: 'input',
         name: 'username',
+        default: 'rrsalerno21',
         message: "What's your GitHub username?"
     },
     {
         type: 'input',
         name: 'repo',
+        default: 'readme-generator',
         message: "What's your project's repo name (include hyphens)?"
     },
     {
         type: 'input',
         name: 'title',
+        default: 'README Generator',
         message: "What's your project's title?"
     },
     {
         type: 'input',
         name: 'description',
+        default: "Command-line app that dynamically generates a README.md from a user's input.  The app is invoked with the following command: node index.js",
         message: "What's your project's description?"
     },
     {
@@ -55,7 +59,7 @@ async function init() {
     try {
         const answers = await promptUser();
         console.log(answers)
-        const markdown = generateMarkdown(answers);
+        const markdown = await generateMarkdown(answers);
         await writeFileAsync('README.md', markdown);
         console.log('Success; checkout the readme file');
     } catch (error) {
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,73 +1,79 @@
 const axios = require('axios');
 
-function generateMarkdown(data) {
-  let email;
-  axios
-    .get(`https://api.github.com/users/${data.title}`)
-    .then(function(res) {
-      email = res.email;
-    })
-  return `
-# ${data.title}
+async function generateMarkdown(data) {
+  try {
+    var userEmail;
+    const gitHubGet = await axios.get(`https://api.github.com/users/${data.username}`)
+    if (gitHubGet.email === null || gitHubGet.email === undefined) {
+      userEmail = '';
+    } else {
+      userEmail = `My Email: ${gitHubGet.email}`;
+    };
 
-![GitHub Last Commit](https://img.shields.io/github/last-commit/${data.username}/${data.repo})
-![GitHub License](https://img.shields.io/github/license/${data.username}/${data.repo})
-
-## Description 
-
-${data.description}
-
-## Table of Contents
-
-* [Installation](#installation)
-* [Usage](#usage)
-* [License](#license)
-* [Contributing](#contributing)
-* [Tests](#tests)
-* [Questions](#questions)
-
-
-## Installation
-
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
-
-
-## Usage 
-
-Provide instructions and examples for use. Include screenshots as needed. 
-
-
-## Credits
-
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
-
-
-
-## License
-
-${data.license}
-
-
-## Contributing
-
-If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
-
-## Tests
-
-Go the extra mile and write tests for your application. Then provide examples on how to run them.
-
-##  Questions
-
-Feel free to contact me!
-
-[![My Profile Photo](https://github.com/rrsalerno21.png)](https://github.com/rrsalerno21)
-My email: ${email}
-
-`;
+    return `
+    # ${data.title}
+    
+    ![GitHub Last Commit](https://img.shields.io/github/last-commit/${data.username}/${data.repo})
+    ![GitHub License](https://img.shields.io/github/license/${data.username}/${data.repo})
+    
+    ## Description 
+    
+    ${data.description}
+    
+    ## Table of Contents
+    
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    
+    
+    ## Installation
+    
+    What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+    
+    
+    ## Usage 
+    
+    Provide instructions and examples for use. Include screenshots as needed. 
+    
+    
+    ## Credits
+    
+    List your collaborators, if any, with links to their GitHub profiles.
+    
+    If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+    
+    If you followed tutorials, include links to those here as well.
+    
+    
+    
+    ## License
+    
+    ${data.license}
+    
+    
+    ## Contributing
+    
+    If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
+    
+    ## Tests
+    
+    Go the extra mile and write tests for your application. Then provide examples on how to run them.
+    
+    ##  Questions
+    
+    Feel free to contact me!
+    
+    [![My Profile Photo](https://github.com/rrsalerno21.png)](https://github.com/rrsalerno21)
+    ${userEmail}
+    
+    `;
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 module.exports = generateMarkdown;
